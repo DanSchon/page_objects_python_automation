ac3ffcc42c779f214cbcf93f51eee6f81225127a
Daniel Schonfeld <danyschonfeld@gmail.com>
2016-12-04 16:34:26 -0800

    swith to iframe method

---
+++ pages/switch_to_iframes/switch_to_iframes.py
@@ -0,0 +1,54 @@
+from selenium.webdriver.common.by import By 
+import utilities.custom_logger as cl
+import logging
+import unittest
+from base.basepage import BasePage
+
+
+class SwitchToIframe(BasePage):
+
+    log = cl.customLogger(logging.DEBUG)
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    # Locators 
+    _iframe_id = "id-iframe"
+    _iframe_name = "name-iframe"
+
+    # Actions
+    
+    def switchToIframeUsingNumbers(self):   # if the iFrame does not have an id or name, use its number to locate it. 
+        self.driver.switch_to.frame(0)      # if there is only one iFrame, its number is 0
+                                            # if there are multiple iFrames, find them by their index number (by order of appearance - ex: higher in the page lower their index number is)
+    def switchToIframeUsingID(self):
+        self.driver.switch_to.frame(self._iframe_id)
+
+    def switchToIframeUsingName(self):
+        self.driver.switch_to.frame(self._iframe_name)
+
+
+    def switchBackToParentFrame(self):
+        self.switch_to.default_content()
+
+    # Methods
+    def switchToIframePerformSomeActionSwitchBackToParentFrame(self):
+        self.switchToIframeUsingNumbers()
+        # The elements of the iframe are now accessible by selenium webdriver 
+        # perform the action
+        searchBox = driver.find_element(By.ID, "search-courses")
+        searchBox.send_keys("python")
+        # Switch back to the parent frame
+        driver.switchBackToParentFrame()
+
+    # Assertions
+    def switchToIframeVerifyThatSearchBoxWorkedSwitchBackToParentFrame(self):
+        self.switchToIframeUsingNumbers()
+        # elements of the iframe are now available for selenium to perform asssertions on 
+        course = self.waitForElement(self._python_course_selector, locatorType="xpath")
+        result = course.is_displayed()
+        self.verify(result, "Passed", "Search box did not work correctly")
+        # Switch back to the parent frame
+        self.switchBackToParentFrame()
+
